f92473cdad8f8e7a43fcfd2c9bffe1c94113f89a
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-06-10 10:14:54 -0700

    save imageURI in filesystem memory

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,3 +1,5 @@
+import * as FileSystem from "expo-file-system";
+
 export const FETCH_PLACES = "FETCH_PLACES";
 export const ADD_PLACE = "ADD_PLACE";
 
@@ -7,9 +9,23 @@ export const fetchPlaces = () => {
   };
 };
 
-export const addPlace = (title, address, imageURI) => {
-  return {
+export const addPlace = (title, address, imageURI) => async (dispatch) => {
+  const fileName = imageURI.split("/").pop();
+
+  const newimageURI = FileSystem.documentDirectory + fileName;
+
+  try {
+    await FileSystem.moveAsync({
+      from: imageURI,
+      to: newimageURI,
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  dispatch({
     type: ADD_PLACE,
-    placeData: { title, address, imageURI },
-  };
+    placeData: { title, address, imageURI: newimageURI },
+  });
 };
